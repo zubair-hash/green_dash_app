44294b7f9f8e1fbb1016fca49d9f1e43fbc0be74
Zubair <muhammad.zubair@dnv.com>
2024-11-05 12:09:40 +0100

    v00.27

--- app.py
+++ app.py
@@ -604,5 +604,6 @@ def update_hs_kde(_):
 
 # Start the app
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run_server(debug=False)
+    # port = int(os.environ.get("PORT", 8050))
+    # app.run_server(host="0.0.0.0", port=port)
